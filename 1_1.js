5d98b62d671ed8073fb52d77564a2bd30f741190
Daniel Lu <dalu@usc.edu>
2015-10-23 20:57:52 -0700

    1.1 concat strings

---
+++ 1_1.js
@@ -0,0 +1,9 @@
+var string1 = "This is a ";
+var string2 = "test";
+
+var string3 = string1 + string2; // creates a new string with "This is a test"
+
+var oldValue = "apples";
+oldValue += " and oranges";
+
+var newString = "".concat("This ","is ","a ","string");
