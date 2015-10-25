aeb6aa78e6f4f7e7b4118ee200cbb8e764733ede
Daniel Lu <dalu@usc.edu>
2015-10-24 19:46:59 -0700

    1.3 String methhod localeCompare

--- 1_3.js
+++ 1_3.js
@@ -13,3 +13,27 @@ if(strName.toUpperCase() == "SHELLEY") {
 	alert("Your name isn't Shelley. Bummer.");
 }
 
+var numVal = 10.0;
+if(numVal == "10") alert("This value is ten");//succeeds
+
+//strict equality operator(===)
+var strObject =  new String("Shelley");
+var strLiteral = "Shelley";
+if(strObject == strLiteral) //this comparison succeeds
+
+if(strObject == strLiteral) //fails because of different data types
+
+//lexically with strings
+var sOne = "cat";
+var sTwo = "dog";
+if(sOne > sTwo) //false
+
+var sOne = "Cat";
+var sTow = "cat";
+if(sOne >= sTwo)// true
+
+//String method localeCompare
+var fruit1 = "apple";
+var fruit2 = "grape";
+var i = fruit1.localeCompare(fruit2); //returns -1
+
